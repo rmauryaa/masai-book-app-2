638ceb119c81a4de85f9c07a594d03dd6e6bc741
rmaurya <rahulmaurya1036@gmail.com>
2024-07-15 19:02:17 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -10,15 +10,9 @@ const createUserTable = async () => {
     )
   `;
 
-  // try {
-  //   const [results, fields] = await db.promise().query(query);
-  //   console.log('User table created or already exists');
-  // } catch (err) {
-  //   console.error('Error creating user table:', err);
-  //   throw err;
-  // }
+
   try {
-    // Ensure using promise-based query
+    
     const [results, fields] = await db.query(query);
     console.log('User table created or already exists');
   } catch (err) {
